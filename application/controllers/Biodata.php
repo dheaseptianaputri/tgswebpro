d913382c3893c840842f5c0578881bc41acebaa6
DheaSeptianaptri <dheaseptianaputri7@gmail.com>
2023-04-01 15:43:33 +0700

    belajar coding

--- application/controllers/Biodata.php
+++ application/controllers/Biodata.php
@@ -3,9 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Biodata extends CI_Controller {
 
-
     public function biodataku()
     {
         $this->load->view('biodata');
+    }
+    public function penjumlahan($nilai1, $nilai2)
+    {
+        echo $this->Model_aritmatika->penjumlahan($nilai1, $nilai2);
     }
 }
